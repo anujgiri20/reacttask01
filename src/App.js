e4cfff3c2096bf03f3addeb4ea47bfca0af6a053
anujgiri20 <87684592+anujgiri20@users.noreply.github.com>
2021-10-04 14:31:58 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() { 
+  
+const plan = [
+  {
+    name:"FREE",
+    price:"$0",
+    features:[
+"Single User",
+"5GB Storage",
+"Unlimited Public Projects",
+"Community Access",
+"Unlimited Private Projects",
+"Dedicated Phone Support",
+"Free Subdomain",
+"Monthly Status Reports"
+    ]
+
+    
+  },
+  {
+    name:"PLUS",
+    price:"$9",
+    features:[
+     " 5 Users",
+     " 50GB Storage",
+     " Unlimited Public Projects",
+     " Community Access",
+     " Unlimited Private Projects",
+     " Dedicated Phone Support",
+     " Free Subdomain",
+     " Monthly Status Reports"
+    ]
+
+    
+  }
+  ,
+  {
+    name:"PRO",
+    price:"$49",
+    features:[
+    " Unlimited Users",
+    " 150GB Storage",
+    "  Unlimited Public Projects",
+    "  Community Access",
+     " Unlimited Private Projects",
+     " Dedicated Phone Support",
+     " Unlimited Free Subdomains",
+     " Monthly Status Reports"
+    ]
+
+    
+  }
+] 
+// the index values is because when we use map that time one error will came so to rempve that we have to set all id to unique and key =index set id for all card to unique
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {plan.map((plan , index)=>(
+      <Card key={index} name={plan.name} price={plan.price} feat={plan.features} />
+    ))
+      }
     </div>
   );
 }
 
-export default App;
+function Card(props)
+{
+return (
+  // <div className="container">
+  //   <div className="row">
+  <div className="card">
+    <p className="head">{props.name}</p>
+    <p className="price">{props.price}<span className="month">/month</span></p>
+   <hr/>
+    <ul className="ul">
+      {props.feat.map((feat,index)=>
+      (<li key={index} className="li">{feat}</li>
+      ))}
+    </ul>
+    <button className="btn">BUY</button>
+  </div>
+  // </div>
+  // </div>
+);
+}
